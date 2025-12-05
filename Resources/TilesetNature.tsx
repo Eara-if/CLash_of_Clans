bfdeb1f0ae172b92de4cc9af6e595b57172c27eb
ttbhh892 <1252971275@qq.com>
2025-12-05 21:42:58 +0800

    Add files via upload

---
+++ Resources/TilesetNature.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TilesetNature" tilewidth="32" tileheight="32" tilecount="120" columns="12">
+ <image source="TilesetNature.png" width="384" height="336"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9.91206" y="9.99606"/>
+  </objectgroup>
+ </tile>
+ <tile id="83">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-14.1961" y="7.89605" height="0.336002"/>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.0801" y="9.99606"/>
+  </objectgroup>
+ </tile>
+</tileset>
