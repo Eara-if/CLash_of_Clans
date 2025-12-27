3c6e2c9dca88c2fc31e8ce45f87d4d8be6909ee0
ttbhh892 <1252971275@qq.com>
2025-12-28 00:31:32 +0800

    Add files via upload

--- BuildingInfoLayer.h
+++ BuildingInfoLayer.h
@@ -1,24 +1,21 @@
-﻿#ifndef BUILDING_INFO_LAYER_H_
-#define BUILDING_INFO_LAYER_H_
+﻿#ifndef __BUILDING_INFO_LAYER_H__   //意味着此代码只会被编译一次，第二次时会跳过define-endif
+#define __BUILDING_INFO_LAYER_H__
 
 #include "cocos2d.h"
-#include <functional> // ����������������ڻص�
-// BuildingInfoLayer.h
+#include <functional> 
 
-
-class Building;
-class BuildingInfoLayer : public cocos2d::Layer
+class Building;   //声明建筑类供后续使用
+class BuildingInfoLayer : public cocos2d::Layer  //继承自 Layer BuildingInfoLayer 是一个容器，可以接收触摸事件
 {
 public:
+    //初始化函数
     virtual bool init();
+   
 
-    // ���������ľ�̬����
+    //CREATE_FUNC自动生成一个静态的 create() 函数
     CREATE_FUNC(BuildingInfoLayer);
 
-    // ���õ�����ʾ����Ϣ
-    // ���������֣���ǰ�ȼ�����������
     void setBuilding(Building* building);
-    void BuildingInfoLayer::showTrainingMenu();
 
     // �رյ���
     void closeLayer();
@@ -28,17 +25,14 @@ private:
     bool _isVisitorMode = false; // 默认为主人模式
     void handleStartUpgrade();
     void handleUpgradeTimer();
-    Building* _targetBuilding = nullptr; // ���浱ǰ���ڲ鿴�Ľ���
-    cocos2d::Sprite* bg = nullptr;
-    cocos2d::Menu* menu = nullptr;
-    cocos2d::Label* _infoLabel; // ��ʾ "Lv.1" �� "Time: 10s"
-    cocos2d::MenuItemFont* _actionBtn; // "Upgrade" �� "Speed Up"
-    bool _isShowingTimer = false;
+    Building* _targetBuilding = nullptr;   //当前选中的建筑
+    cocos2d::Sprite* bg = nullptr;     //背景图片
+    cocos2d::Menu* menu = nullptr;     //放按钮的菜单
+    cocos2d::Label* _infoLabel;        //主信息文本
+    cocos2d::MenuItemFont* _actionBtn;  //主功能按钮
+    bool _isShowingTimer = false;      //当前是否在倒计时
 
-    // ����������ʾ���ȼ�����
     void showMaxLevelWarning(const std::string& buildingName, int townHallLevel);
-
-    // ����������ʾ����Ҫ�󾯸�
     void showLockedWarning(const std::string& buildingName, int requiredTHLevel);
 };
 
